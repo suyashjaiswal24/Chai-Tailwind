6ee0621b423f08bbb382b6a1a571c2ac1990a2b5
Suyash Jaiswal <simple.sjaiswal@gmail.com>
2026-03-22 02:36:34 +0530

    Handled multiple elements

--- main.js
+++ main.js
@@ -7,30 +7,33 @@ const styles = {
 }
 
 
-let ele = document.querySelectorAll('[class]')
-console.log("Elements: ", ele) // Elements:  NodeList(2) [h1.chai-border-10.chai-color-green, h1.heading]
-let ele2 = document.querySelectorAll('[class *= "chai-"]')
-console.log("Elements: ", ele2) // Elements:  NodeList [h1.chai-border-10.chai-color-green]
+let ele = document.querySelectorAll('[class *= "chai-"]')
+console.log("Elements: ", ele) // Elements:  NodeList(3) [h1.chai-border-10.chai-color-green, h1.chai-color-blue, h1.nochai-color-blue]
 
-const classArray = Array.from(ele2[0].classList)
-console.log("ClassArray: ", classArray) // ClassArray:  (2) ['chai-border-10', 'chai-color-green']
-
-let style = ""
-
-classArray.forEach((cl) => {
+ele.forEach(e => {
+    const classArray = Array.from(e.classList)
+    console.log("ClassArray: ", classArray) // ClassArray:  (2) ['chai-border-10', 'chai-color-green']    ClassArray:  ['chai-color-blue']    ClassArray:  ['nochai-color-blue']
     
-    const parts = cl.split("-") // parts = ["chai", "border", "10"]       parts = ["chai", "color", "green"]
-
-    const type = parts[1] // border      color
-    const value = parts[2] // 10         green
-
-    if(styles[type]){
-        console.log(`Style for ${type}-> `, styles[type](value) + "; ") //Style for border->  border: 10px solid red;           Style for color->  color: green; 
-        style += styles[type](value) + "; "
-        console.log( `Element Style becomes-> ` , style) // Element Style becomes->  border: 10px solid red;      Element Style becomes->  border: 10px solid red; color: green;
-    }
-})
-
-ele2[0].style = style // <h1 class="chai-border-10 chai-color-green" style="border: 10px solid red; color: green;">Hello ji</h1>
+    const chaiList = classArray.filter((c) => c.startsWith("chai-"))
+    console.log("ChaiList: ", chaiList) // ChaiList:  (2) ['chai-border-10', 'chai-color-green']    ChaiList:  ['chai-color-blue']    ChaiList:  []
+    
+    let style = ""
+    
+    chaiList.forEach((ch) => {
+        console.log("Chai Element's class: ",ch)
+        const parts = ch.split("-") // parts = ["chai", "border", "10"]    parts = ["chai", "color", "green"]   parts = ["chai", "color", "blue"]
+    
+        const type = parts[1] // border  color      color
+        const value = parts[2] // 10     green      blue
+    
+        if(styles[type]){
+            console.log(`Style for ${type}-> `, styles[type](value) + "; ") //Style for border->  border: 10px solid red;    Style for color->  color: green;    Style for color->  color: blue; 
+            style += styles[type](value) + "; "
+            console.log( `Element Style becomes-> ` , style) // Element Style becomes->  border: 10px solid red;   Element Style becomes->  border: 10px solid red; color: green;      Element Style becomes->  color: blue; 
+        }
+    })
+    
+    e.style.cssText += style // <h1 class="chai-border-10 chai-color-green" style="border: 10px solid red; color: green;">Hello ji</h1>       <h1 class="chai-color-blue" style="color: blue;">Hello ji</h1>
+});
 
 
